d7fe5edefe42c10d6d559c4165f6067e80c53c60
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-08-06 13:53:16 -0700

    search on enter

--- scripts/frontScript.js
+++ scripts/frontScript.js
@@ -99,47 +99,53 @@ $(document).ready(function () {
     $("#recentTable").prepend("<tr>" + snap.val().thisAll + "</tr>");
   });
 
+  //set enter in seach to click button
+  $("#input").on("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      $('#submit').click();
+    }
+  });
+
   // Created for an Articles on:
   // https://www.html5andbeyond.com/bubbling-text-effect-no-canvas-required/
 
-  jQuery(document).ready(function ($) {
 
-    // Define a blank array for the effect positions. This will be populated based on width of the title.
-    var bArray = [];
-    // Define a size array, this will be used to vary bubble sizes
-    var sArray = [4, 6, 8, 10];
+  // Define a blank array for the effect positions. This will be populated based on width of the title.
+  var bArray = [];
+  // Define a size array, this will be used to vary bubble sizes
+  var sArray = [4, 6, 8, 10];
 
-    // Push the header width values to bArray
-    for (var i = 0; i < $('.bubbles').width(); i++) {
-      bArray.push(i);
-    }
+  // Push the header width values to bArray
+  for (var i = 0; i < $('.bubbles').width(); i++) {
+    bArray.push(i);
+  }
 
-    // Function to select random array element
-    // Used within the setInterval a few times
-    function randomValue(arr) {
-      return arr[Math.floor(Math.random() * arr.length)];
-    }
+  // Function to select random array element
+  // Used within the setInterval a few times
+  function randomValue(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
-    // setInterval function used to create new bubble every 350 milliseconds
-    setInterval(function () {
+  // setInterval function used to create new bubble every 350 milliseconds
+  setInterval(function () {
 
-      // Get a random size, defined as variable so it can be used for both width and height
-      var size = randomValue(sArray);
-      // New bubble appeneded to div with it's size and left position being set inline
-      // Left value is set through getting a random value from bArray
-      $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+    // Get a random size, defined as variable so it can be used for both width and height
+    var size = randomValue(sArray);
+    // New bubble appeneded to div with it's size and left position being set inline
+    // Left value is set through getting a random value from bArray
+    $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
 
-      // Animate each bubble to the top (bottom 100%) and reduce opacity as it moves
-      // Callback function used to remove finsihed animations from the page
-      $('.individual-bubble').animate({
-        'bottom': '100%',
-        'opacity': '-=0.7'
-      }, 3000, function () {
-        $(this).remove();
-      });
+    // Animate each bubble to the top (bottom 100%) and reduce opacity as it moves
+    // Callback function used to remove finsihed animations from the page
+    $('.individual-bubble').animate({
+      'bottom': '100%',
+      'opacity': '-=0.7'
+    }, 3000, function () {
+      $(this).remove();
+    });
 
 
-    }, 350);
+  }, 350);
 
-  });
 });
